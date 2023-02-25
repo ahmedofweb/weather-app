ad77cb33c5f4c5a48476250ff8b5af5fb4478ca2
ahmedofweb <abdulxamidweb@gmail.com>
2023-02-25 16:03:48 +0300

    weather app

---
+++ js/app.js
@@ -0,0 +1,62 @@
+const changeLocation = document.getElementById('change-location')
+const card = document.getElementById('card')
+const details = document.getElementById('details')
+const weatherIcon = document.getElementById('weather-icon')
+const body = document.body
+const overlay = document.getElementById('overlay')
+
+
+// sitega kirilganda focus inputga qoyilishi
+changeLocation.city.focus()
+
+
+// loader
+function loader(state){
+    if(state){
+        overlay.classList.remove('d-none')
+    }else{
+        overlay.classList.add('d-none')
+    }
+}
+
+// card innerHtmlining ozgarishi
+const updateUI = (obhavo) =>{
+    const {name , weather, main } = obhavo
+    console.log('updateUI:', obhavo)
+    details.innerHTML = `
+    <h5 class="mb-3">${name}</h5>
+    <p class="mb-3">${weather[0].main}</p>
+    <div class="display-4 mb-3">
+      <span>${Math.round(main.temp)}</span>
+      <span>&deg;C</span>
+    </div>
+    `
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    }
+
+    // weatherIcon.src = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+    weatherIcon.src = `http://openweathermap.org/img/wn/10d@2x.png`
+}
+
+
+// obhavo malumotlarini forecast.js dan olinishi
+const getWeather = async (city)=>{
+    const data = await getData(city)
+    
+
+    return data
+}
+
+
+// forma submit bolganda malumot kelishi
+changeLocation.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const cityName = changeLocation.city.value.trim()
+    changeLocation.reset()
+    getWeather(cityName).then((data)=> updateUI(data))
+    
+})
+
+
